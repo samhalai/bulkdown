40bc69b2be44b3d37f59dfa499b9dd1282bb80e9
Sameer Halai <sameer@sameerhalai.com>
2013-07-05 09:34:47 -0700

    Creates a directory if none exists

--- grabImages.pl
+++ grabImages.pl
@@ -2,14 +2,24 @@
 
 use strict;
 use warnings;
-use DateTime;
 
+use DateTime;
+use LWP::Simple;
+
+my $storeFolder = "store";
 my $base_url = "http://www.pscleanair.org/airq/visibility/seattleqa/webcam-";
 my $extension = ".jpg";
 my $time_of_day = "15";
+my $filename = "";
 my $now = DateTime->now;
-my $start_of_last_year = DateTime->new( year => $now->year - 1);
+my $start_of_last_year = DateTime->new( year => $now->year- 1);
+
+unless(mkdir $storeFolder) {
+		die "Unable to create $storeFolder\n";
+}
+
 while ($now > $start_of_last_year) {
-	print $base_url, $now->ymd(''), $time_of_day, $extension, "\n";
+	$filename = $now->ymd('').$time_of_day.$extension;
+	print $base_url, $filename, "\n";
 	$now->subtract( days => 1);
 }
